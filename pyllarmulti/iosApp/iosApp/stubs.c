6623c13df5f7952b6e7c343f6720da9e51dd86da
Aneez <aneezjos19@gmail.com>
2026-07-15 18:40:07 +0530

    stubs

---
+++ pyllarmulti/iosApp/iosApp/stubs.c
@@ -0,0 +1,8 @@
+// Stubs to resolve undefined Skia/Skiko symbols when DEAD_CODE_STRIPPING is disabled
+// Since the iOS app never invokes these optional SVG / TextBlob features, 
+// these stubs will never be executed.
+
+void org_jetbrains_skia_TextBlobBuilderRunHandler__1nGetFinalizer(void) {}
+void org_jetbrains_skia_TextBlobBuilderRunHandler__1nMake(void) {}
+void org_jetbrains_skia_TextBlobBuilderRunHandler__1nMakeBlob(void) {}
+void org_jetbrains_skia_svg_SVGCanvasKt__1nMake(void) {}
